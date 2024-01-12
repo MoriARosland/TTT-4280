Fixed incorrect values

oppg1a.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-maalinger = [20.6, 20.4, 20.4, 20.6, 20.4, 20.8, 20.5, 20.5, 20.5,
-             20.4, 20.5, 20.5, 20.5, 20.5, 20.4, 20.4, 20.4, 20.5, 20.3, 20.6]
+maalinger = np.array([20.6, 20.4, 20.4, 20.6, 20.4, 20.8, 20.5, 20.5, 20.5,
+                      20.4, 20.5, 20.5, 20.5, 20.5, 20.4, 20.4, 20.4, 20.5, 20.3, 20.6])
 
 
 avg = sum(maalinger)/20
 
-varians = np.var(maalinger)
+varians = 1/(20-1)*np.sum((maalinger-avg)**2)
 standard_avvik = np.sqrt(varians)
 
 print(varians)

oppg1b.py
@@ -2,7 +2,7 @@ import numpy as np
 
 avg = 20.485
 maalinger = 20
-standard_avvik = 0.106  # Fra oppgave 1b
+standard_avvik = 0.1089  # Fra oppgave 1b
 t_p = 2.093  # Fra tosidig student t-fordeling
 
 konfidensintervall_pos = avg + t_p*(standard_avvik/np.sqrt(maalinger))