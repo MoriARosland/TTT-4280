87589bdf0b846a49248d444fac4217745f951247
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-12 13:52:24 +0100

    add task 2c

---
+++ oppgave2c.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+intervall1 = np.array([20.6, 20.4, 20.4, 20.6, 20.4,
+                      20.8, 20.5, 20.5, 20.5, 20.4])
+
+intervall2 = np.array([20.4, 20.4, 20.4, 20.2, 20.4,
+                      20.3, 20.4, 20.5, 20.4, 20.4,])
+
+# def beregn_gjennomsnsitt(maalinger):
